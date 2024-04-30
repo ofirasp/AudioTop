b2ab3272ea40206063b9d02b81c23fac503b649d
ofirasp <35403152+ofirasp@users.noreply.github.com>
2024-04-30 15:46:06 +0300

    version 0.0.3 add dsd add factor

--- PeppyMeter/circular.py
+++ PeppyMeter/circular.py
@@ -53,7 +53,7 @@ class CircularAnimator(object):
         
         :return: list of rectangles for update
         """
-        volume = max(self.get_data()*addfactor,100)
+        volume = min(self.get_data()*addfactor,100)
         n, a = self.set_sprite(volume)
         self.previous_index = int(n)
 
